74229028a401cdfe7d96cf05c1e4d342c17938bb
npham014 <npham014@ucr.edu>
2021-08-10 16:45:31 -0700

    changed navbar info to hover over

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,108 @@
 import "./navbar.scss";
+import React from "react";
 import {Phone, Mail, LinkedIn} from "@material-ui/icons";
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
+import Popover from '@material-ui/core/Popover';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    popover:{
+        pointerEvents: "none"
+    },
+    paper: {
+        padding:theme.spacing(1)
+    }
+}));
 
 export default function Navbar() {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [openPopoverId, setOpenPopoverId] = React.useState(null);
+    const handlePopoverOpen = (event, pId) => {
+        setAnchorEl(event.target);
+        setOpenPopoverId(pId);
+    };
+    
+    const handlePopoverClose = () => {
+        setAnchorEl(null);
+        setOpenPopoverId(null);
+    };
+    
+
     return (
         <div className="navbar">
             <div className="wrapper">
                 <div className="left">
                     <a href="#about" className="logo">me</a>
-                    <div className="itemContainer"> {/*TODO: CHANGE ICONS TO BUBBLE ON CLICK*/}
-                        <Phone className="icon"/>
-                        <span>+1 (626) 800-7175</span>
+                    <div className="itemContainer">
+                        <Phone 
+                            className="icon"
+                            onMouseEnter={(e) => {handlePopoverOpen(e, "phonePopover")}}
+                            onMouseLeave={handlePopoverClose}
+                        />
+                        <Popover
+                            id="phonePopover"
+                            className={classes.popover}
+                            classes={{
+                                paper: classes.paper
+                            }}
+                            open={openPopoverId === "phonePopover"}
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                                vertical: "bottom",
+                                horizontal: "right"
+                            }}
+                            transformOrigin={{
+                                vertical:"top",
+                                horizontal:"left"
+                            }}
+                            onClose={handlePopoverClose}
+                            disableRestoreFocus
+                        >
+                            <Typography>+1-(626)-800-7175</Typography>
+                        </Popover>
                     </div>
                     <div className="itemContainer">
-                        <Mail className="icon"/>
-                        <span>natpham014@gmail.com</span>
+                        <Mail 
+                            className="icon"
+                            onMouseEnter={(e) => {handlePopoverOpen(e, "mailPopover")}}
+                            onMouseLeave={handlePopoverClose}
+                        />
+                        <Popover
+                            id="mailPopover"
+                            className={classes.popover}
+                            classes={{
+                                paper: classes.paper
+                            }}
+                            open={openPopoverId === "mailPopover"}
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                                vertical: "bottom",
+                                horizontal: "right"
+                            }}
+                            transformOrigin={{
+                                vertical:"top",
+                                horizontal:"left"
+                            }}
+                            onClose={handlePopoverClose}
+                            disableRestoreFocus
+                        >
+                            <Typography>natpham014@gmail.com</Typography>
+                        </Popover>
                     </div>
                     <div className="itemContainer">
-                        <a href="https://www.linkedin.com/in/nathan-pham-186a2b14b/" className="icon"> <LinkedIn className="liIcon"/> </a>
+                        <IconButton href="https://www.linkedin.com/in/nathan-pham-186a2b14b/" target="_blank">
+                            <LinkedIn className="liIcon"/>
+                        </IconButton>
                     </div>
                 </div>
                 
-                <div clasName="right">
-                    right
+                <div className="right">
+                    <IconButton className="menuButton">
+                        <MenuIcon className="icon"/>
+                    </IconButton>
                 </div>
             </div>
         </div>
